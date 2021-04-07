a993f44f7b6d5726aabb8dc4066a9c2afdc936ed
qaser <huji@mail.ru>
2021-04-07 22:34:58 +0500

    add users app

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -1,21 +1,26 @@
-"""foodgram URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
+from django.conf.urls import handler404, handler500
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+# from django.contrib.flatpages import views
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('master-chief/', admin.site.urls),
+    path('auth/', include('users.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
+    path('', include('recipes.urls')),
 ]
+
+# urlpatterns += [
+#     path('about-us/', views.flatpage, {'url': '/about-us/'}, name='about'),
+#     path('terms/', views.flatpage, {'url': '/terms/'}, name='terms'),
+#     path('about-spec/', views.flatpage, {'url': '/about-spec/'}, name='spec'),
+#     path('about-author/', views.flatpage, {'url': '/about-author/'}, name='author'),
+# ]
+
+if settings.DEBUG:
+#    import debug_toolbar
+#    urlpatterns += (path("__debug__/", include(debug_toolbar.urls)),)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
