85d710a9136278ad1733a7d3944b3a9d2b7c9018
qaser <huji@mail.ru>
2021-06-07 17:52:51 +0500

    very-final version: fix settings

--- recipes/views.py
+++ recipes/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.cache import cache_page
 
 from .forms import RecipeForm
-from .models import Ingredient, Recipe, Subscription, User
+from .models import Recipe, Subscription, User
 from .utils import check_paginator, split_on_page
 
 
@@ -26,7 +26,7 @@ def profile(request, username):
     user = get_object_or_404(User, username=username)
     recipes = Recipe.objects.filter(
         author=user
-    ).select_related('author').get_by_tags(tag=active_tags,user=request.user)
+    ).select_related('author').get_by_tags(tag=active_tags, user=request.user)
     selection = split_on_page(request, recipes)
     context = {
         'filters': request.META['url_tail_tags'],
